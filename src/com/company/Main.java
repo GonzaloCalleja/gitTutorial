3b9eeba71d596e9d7145fedcd2f656fdb2fad470
Gonzalo <gonzacalleja@gmail.com>
2019-10-13 23:31:28 +0200

    Copy & Paste Phonebook program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+        System.out.println("PHONE AGENDA");
+        System.out.print("Number of contacts: ");
+        int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Contact name & number: ");
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name, phone);
+        }
+
+        System.out.println("Name people you want to search for: ");
+
+        while (in.hasNext()) {
+            String s = in.next();
+            if (phoneBook.get(s) != null) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
     }
 }
